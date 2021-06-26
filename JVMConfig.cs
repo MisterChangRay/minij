b0b91522d202bca856617981031e64671fd0b8e5
Your Name <you@example.com>
2021-06-26 12:02:25 +0800

    first init

---
+++ JVMConfig.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minij
+{
+    public class JVMConfig
+    {
+        public static JVMConfig config;
+
+        [Option(shortName: 'v', longName:"verbose", Required = false, HelpText = "set out to verbose message")]
+        public bool verbose { get; set; }
+        [Option(longName: "cp", Required = false, HelpText = "set classpath")]
+        public string cp { get; set; }
+        [Option(longName:"classpath", Required = false, HelpText = "set classpath")]
+        public string classpath { get; set; }
+        [Option(longName:"jrehome", Required = false, HelpText = "set jre home dir")]
+        public string jrehome { get; set; }
+
+        [Option(longName: "mainClass", Required = false, HelpText = "set main class")]
+        public string mainClass { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mrui
+ * Date: 2021/6/26
+ * Time: 9:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using CommandLine;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace minij
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+				
+            parseArgs(args);
+            if(args.Length > 0)
+            {
+                JVMConfig.config.mainClass = args[0];
+            }
+            if (JVMConfig.config.verbose) {
+                Console.WriteLine("打开详细输出模式");
+                Console.WriteLine("启动类:" + JVMConfig.config.mainClass);
+                Console.WriteLine("用户JRE目录:" + JVMConfig.config.jrehome);
+            }
+            
+
+            Console.ReadKey(true);
+		}
+
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
+        }
+        public static void parseArgs(string[] args) {
+            CommandLine.Parser.Default.ParseArguments<JVMConfig>(args).WithParsed(o =>
+            {
+                JVMConfig.config = o;
+            }).WithNotParsed(o => {
+            });
+		}
+	}
+}
